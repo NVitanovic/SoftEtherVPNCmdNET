6ce66143504778ea942fbe814bd03772cd85e7ca
NVitanovic <x-team@live.com>
2018-04-14 15:37:39 +0200

    Base wrapper for vpncmd added
    Base wrapper needed for execution is added, also xUnit tests are added
    in order to be able to test the code as we write it.

---
+++ SoftEtherVPNCmdNETCore/SoftEtherVPNCmdNETCore/VPNClient/IVPNClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftEtherVPNCmdNETCore.VPNClient
+{
+    interface iVPNClient
+    {
+        string About();
+        Dictionary<string,string> VersionGet();
+        void PasswordSet(string password, bool remoteOnly = false);
+        Dictionary<string, string> PasswordGet();
+    }
+}
---
+++ SoftEtherVPNCmdNETCore/XUnitSoftEtherVPNCmdNETCore/SoftEtherVPNCmdNetCoreTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using SoftEtherVPNCmdNETCore;
+
+namespace XUnitSoftEtherVPNCmdNETCore
+{
+    public class SoftEtherVPNCmdNetCoreTests
+    {
+        [Fact]
+        public void CheckSettingOfBinary()
+        {
+            cSoftEtherVPNCmdNETCore cmd = new cSoftEtherVPNCmdNETCore("vpncmd");
+            Assert.Equal(cmd.Binary, "vpncmd");
+        }
+        [Fact]
+        public void CheckExecution()
+        {
+            cSoftEtherVPNCmdNETCore cmd = new cSoftEtherVPNCmdNETCore("vpncmd");
+            var output = cmd.ExecuteCommand("127.0.0.1", "CLIENT", "VersionGet", "");
+            Assert.Contains(",SoftEther VPN Client", output);
+        }
+        [Fact]
+        public void CheckParse()
+        {
+            string output = "Item,Value\nTestItem,TestValue";
+            cSoftEtherVPNCmdNETCore cmd = new cSoftEtherVPNCmdNETCore("vpncmd");
+            var res = cmd.ParseCommand(output);
+            Assert.Equal(1, res.Count);
+            foreach(var pair in res)
+            {
+                Assert.Equal(pair.Key, "TestItem");
+                Assert.Equal(pair.Value, "TestValue");
+            }
+        }
+    }
+}
